4c2618d15782f71c8dfce05cbdcbeca001d02c67
septianmalela <maulanas430@gmail.com>
2021-01-19 09:13:31 +0700

    Testing git

--- app/models/tabungan.rb
+++ app/models/tabungan.rb
@@ -1,8 +1,13 @@
 class Tabungan < ApplicationRecord
 	belongs_to :bunga
+	include ActionView::Helpers::NumberHelper
 
 	def format
-		number_to_currency(example_price, unit: "Rp. ", separator: ",", delimiter: ".")
+		number_to_currency(biaya_perbulan, unit: "Rp. ", separator: ",", delimiter: ".")
+	end
+
+	def format2
+		number_to_currency(target_dana, unit: "Rp. ", separator: ",", delimiter: ".")
 	end
 
 end
